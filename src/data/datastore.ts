ce3482729aa777e38d99735fe34598056d0db010
Andi <andy.u789@gmail.com>
2024-08-12 01:13:45 +0300

    asdqwdsad

--- src/data/datastore.ts
+++ src/data/datastore.ts
@@ -2,16 +2,6 @@ export default function getProjects(): Project[] {
   return [
     {
       id: 'p1',
-      name: 'FindVibe',
-      description:
-        "A PWA web for mobile. Let's your find music, add to favorites and listen offline.",
-      image: '/images/find-vibe.png',
-      url: 'https://find-vibe.vercel.app/',
-      github: 'https://github.com/andiq123/FindVibeFrontEnd',
-      tags: ['Angular', 'Dotnet', 'TypeScript', 'Tailwind CSS'],
-    },
-    {
-      id: 'p2',
       name: 'Chator',
       description:
         'A web app for chatting with friends. Users to test:user1 Password1@ user2 Password1@',
@@ -19,6 +9,16 @@ export default function getProjects(): Project[] {
       url: 'https://chator-andyu789.b4a.run/',
       github: 'https://github.com/andiq123/Chator',
       tags: ['Angular', 'Dotnet', 'TypeScript', 'Bootstrap'],
+    },
+    {
+      id: 'p2',
+      name: 'FindVibe',
+      description:
+        "A PWA web for mobile. Let's your find music, add to favorites and listen offline.",
+      image: '/images/find-vibe.png',
+      url: 'https://find-vibe.vercel.app/',
+      github: 'https://github.com/andiq123/FindVibeFrontEnd',
+      tags: ['Angular', 'Dotnet', 'TypeScript', 'Tailwind CSS'],
     },
     {
       id: 'p3',
@@ -36,6 +36,15 @@ export default function getProjects(): Project[] {
       image: '/images/car-sell.jpeg',
       url: 'https://bucurel-auto.it/',
       github: 'https://github.com/andiq123/bucurel',
+      tags: ['Next.js', 'TypeScript', 'Tailwind CSS'],
+    },
+    {
+      id: 'p5',
+      name: 'Bam-Traslochi',
+      description: 'A website for a moving company',
+      image: '/images/bam-traslochi.png',
+      url: 'https://bam-traslochi-numxvw4hj-andiq123.vercel.app/',
+      github: 'https://github.com/andiq123/bam-traslochi',
       tags: ['Next.js', 'TypeScript', 'Tailwind CSS'],
     },
   ];
